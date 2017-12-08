da73c10f24a683103ea1bd7052b0e4ad6251c0eb
Igor Kulman <igor@kulman.sk>
2017-12-08 12:56:35 +0100

    ♻️ project cleanup

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -16,7 +16,7 @@ lexer.getNextToken()
 let interpeter = Interpreter("7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)")
 interpeter.eval()
 
-let parser = Parser("2 * (7 + 3) ")
+let parser = Parser("5 - - -2")
 print(parser.expr())
 
 let rpn = RPN("(5 + 3) * 12 / 3")
