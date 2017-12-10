c1fc07ac25a68d631e72515154695ef73d50b46a
Igor Kulman <igor@kulman.sk>
2017-12-10 19:43:59 +0100

    ♻️ retargeting the project from iOS to macOS

---
+++ PascalInterpreter/PascalInterpreter/PascalInterpreter.h
@@ -0,0 +1,19 @@
+//
+//  PascalInterpreter.h
+//  PascalInterpreter
+//
+//  Created by Igor Kulman on 10/12/2017.
+//  Copyright © 2017 Igor Kulman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PascalInterpreter.
+FOUNDATION_EXPORT double PascalInterpreterVersionNumber;
+
+//! Project version string for PascalInterpreter.
+FOUNDATION_EXPORT const unsigned char PascalInterpreterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PascalInterpreter/PublicHeader.h>
+
+
--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import Foundation
-import SwiftPascalInterpreter
+import PascalInterpreter
 
 let lexer = Lexer("BEGIN a := 2; END.")
 lexer.getNextToken()
